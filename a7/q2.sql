b4026cebe949f6ffa1f3cbc961c44bd27c00e3b1
yunke zhu <yunke.zhu@tufts.edu>
2020-04-08 22:23:25 -0400

    q2 modified

--- a7/q2.sql
+++ a7/q2.sql
@@ -1,5 +1,4 @@
-create index on r(r_1p);
-create index on r(r_10p);
+create index on r(r_1p,r_10p,r_50p);
 analyze r;
 
 explain analyze
@@ -9,5 +8,4 @@ where r_1p = 0
 and r_10p = 0
 and r_50p = 0;
 
-drop index r_r_1p_idx;
-drop index r_r_10p_idx;
+drop index r_r_1p_r_10p_r_50p_idx;
